2efe522327a65bfb2d05ae93e4ef23bfd309b2bf
Dullstar <dullstar2xqt@gmail.com>
2021-08-27 21:29:38 -0500

    Adding build system, main loop, and keyboard input translation system

---
+++ source/app.d
@@ -0,0 +1,137 @@
+import std.stdio;
+import std.string;
+import allegro5.allegro;
+
+import controller;
+
+void initialize(bool test, string what)
+{
+	if (!test)
+	{
+		stderr.writeln("Error: failed to initialize " ~ what);
+	}
+}
+
+class MainGame
+{
+public:
+	this(int size_x, int size_y)
+	{
+		display = al_create_display(size_x, size_y);
+		initialize((display !is null), "display");
+		timer = al_create_timer(1 / 60.0);
+		initialize((timer !is null), "timer");
+		queue = al_create_event_queue();
+		initialize((queue !is null), "event queue");
+
+		al_register_event_source(queue, al_get_keyboard_event_source());
+		al_register_event_source(queue, al_get_display_event_source(display));
+		al_register_event_source(queue, al_get_timer_event_source(timer));
+
+		main_buffer = al_create_bitmap(size_x, size_y);
+		kb_controller = new KeyboardController("");
+		al_start_timer(timer);
+		// Font.create_fonts();
+		// game_state = new GameState(main_buffer, &kb_controller);
+	}
+	~this()
+	{
+		al_destroy_display(display);
+		al_destroy_timer(timer);
+		al_destroy_event_queue(queue);
+		// Font.destroy_fonts();
+	}
+	void run()
+	{
+		bool redraw = false;
+		int ticks = 0;
+		ALLEGRO_EVENT event;
+		while (true)
+		{
+			al_wait_for_event(queue, &event);
+			switch (event.type)
+			{
+			case ALLEGRO_EVENT_TIMER:
+				++ticks;
+				if (ticks == 1)
+				{
+					update();
+					redraw = true;
+				}
+				break;
+			case ALLEGRO_EVENT_KEY_DOWN:
+				writeln("Key down event: ", event.keyboard.keycode);
+				kb_controller.interpret_down(event.keyboard.keycode);
+				break;
+			case ALLEGRO_EVENT_KEY_CHAR:
+				writeln("Key char event: ", event.keyboard.keycode);
+				kb_controller.interpret_char(event.keyboard.keycode);
+				break;
+			case ALLEGRO_EVENT_KEY_UP:
+				writeln("Key up event: ", event.keyboard.keycode);
+				kb_controller.interpret_release(event.keyboard.keycode);
+				break;
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				return;
+			default:
+				// we can safely ignore any events we don't need to explicitly handle
+				break;
+			}
+			if (redraw && al_is_event_queue_empty(queue))
+			{
+				draw();
+				ticks = 0;
+			}
+		}
+	}
+private:
+	ALLEGRO_TIMER* timer;
+	ALLEGRO_EVENT_QUEUE* queue;
+	ALLEGRO_DISPLAY* display;
+	ALLEGRO_BITMAP* main_buffer;
+	KeyboardController kb_controller;
+	// GameState game_state;
+	void update()
+	{
+		// TEMP
+		foreach (i, key; kb_controller.pressed)
+		{
+			if (key) writeln("Pressed: ", i);
+		}
+		foreach (i, key; kb_controller.chars)
+		{
+			if (key) writeln("Char: ", i);
+		}
+		foreach (i, key; kb_controller.held)
+		{
+			if (key) writeln("Held: ", i);
+		}
+		foreach (i, key; kb_controller.released)
+		{
+			if (key) writeln("Released: ", i);
+		}
+		// END TEMP
+		// game_state.update();
+		kb_controller.prep_for_next_frame();
+	}
+	void draw()
+	{
+		al_set_target_bitmap(main_buffer);
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+		// game_state.draw();
+		al_set_target_bitmap(al_get_backbuffer(display));
+		al_draw_bitmap(main_buffer, 0, 0, 0);
+		al_flip_display();
+	}
+}
+
+int main()
+{
+	return al_run_allegro({
+		initialize(al_init(), "Allegro");
+		initialize(al_install_keyboard(), "keyboard");
+		auto main_game = new MainGame(640, 480);
+		main_game.run();
+		return 0;
+	});
+}
---
+++ source/controller.d
@@ -0,0 +1,167 @@
+module controller;
+import allegro5.allegro;
+import std.conv;
+import std.stdio;
+import std.string;
+
+enum command {
+	up = 0,
+	down = 1,
+	left = 2,
+	right = 3,
+	none
+}
+
+interface Controller
+{
+public:
+	@property ref bool[command.max] released();
+	@property ref bool[command.max] held();
+	@property ref bool[command.max] pressed();
+	@property ref bool[command.max] chars();
+}
+
+class KeyboardController : Controller
+{
+public:
+	this(string config)
+	{
+		if (config != "")
+	   	{
+			try
+			{
+				auto config2 = config.strip().split("\n");
+				foreach(line; config2)
+				{
+					auto line_contents = line.strip().split(", ");
+					size_t keycode = line_contents[0].to!size_t;
+				}
+			}
+			catch(Exception e)
+			{
+				stderr.writeln("Error reading control configuration:\n    ", e);
+				stderr.writeln("    The default controls will be used.");
+				clear_translation_table();
+				init_default_controls();
+			}
+		}
+		else {
+			init_default_controls();
+		}
+	}
+	@property override ref bool[command.max] held()
+	{
+		return translated_key_held;
+	}
+	@property override ref bool[command.max] pressed()
+	{
+		return translated_key_downs;
+	}
+	@property override ref bool[command.max] released()
+	{
+		return translated_key_released;
+	}
+	@property override ref bool[command.max] chars()
+	{
+		return translated_key_chars;
+	}
+	void interpret_down(int keycode)
+	{
+		command translated = translation_table[keycode];
+		if (translated == command.none) return;
+		if (number_keys_held[translated] == 0)
+		{
+			// The condition prevents extra key downs from being generated
+			// if the user has multiple keybinds. Guarantees that no matter
+			// how we interpret the keypress data later, there won't be any
+			// unexpected behavior resulting from users binding multiple
+			// keys to the same function.
+			translated_key_downs[translated] = true;
+		}
+		number_keys_held[translated] += 1;
+		translated_key_held[translated] = true;
+	}
+	void interpret_char(int keycode)
+	{
+		command translated = translation_table[keycode];
+		if (translated == command.none) return;
+		translated_key_chars[translated] = true;
+	}
+	void interpret_release(int keycode)
+	{
+		command translated = translation_table[keycode];
+		if (translated == command.none) return;
+		number_keys_held[translated] -= 1;
+		if (number_keys_held[translated] == 0)
+		{
+			translated_key_released[translated] = true;
+			// Why such complication regarding released keys?
+			// This check, along with the processing in prep_for_next_frame(),
+			// ensures that the key isn't regarded as being held on the frame
+			// that it is released, except if it is necessary to guarantee all
+			// key presses are registered as held for at least one frame.
+			
+			// If it somehow becomes a performance issue, then we could have a
+			// simpler input system that simply assumes that a keypress will
+			// last more than one frame, as this is a solution to a mostly
+			// theoretical problem: I wasn't able to produce sufficiently short
+			// keypresses to be missed without slowing the framerate down.
+			if (!translated_key_downs[translated])
+			{
+				translated_key_held[translated] = false;
+			}
+		}
+	}
+	void prep_for_next_frame()
+	{
+		for (size_t i = 0; i < translated_key_held.length; ++i)
+		{
+			if (translated_key_released[i])
+		   	{
+				translated_key_held[i] = false;
+			}
+		}
+		translated_key_downs[] = false;
+		translated_key_released[] = false;
+		translated_key_chars[] = false;
+	}
+private:
+	bool[command.max] translated_key_downs;
+	bool[command.max] translated_key_released;
+	bool[command.max] translated_key_held;
+	bool[command.max] translated_key_chars;
+	int[command.max] number_keys_held;
+	command[ALLEGRO_KEY_MAX] translation_table = command.none;
+
+	void init_default_controls()
+	{
+		translation_table[ALLEGRO_KEY_W] = command.up;
+		translation_table[ALLEGRO_KEY_UP] = command.up;
+		translation_table[ALLEGRO_KEY_A] = command.left;
+		translation_table[ALLEGRO_KEY_LEFT] = command.left;
+		translation_table[ALLEGRO_KEY_S] = command.down;
+		translation_table[ALLEGRO_KEY_DOWN] = command.down;
+		translation_table[ALLEGRO_KEY_D] = command.right;
+		translation_table[ALLEGRO_KEY_RIGHT] = command.right;
+	}
+	void clear_translation_table()
+	{
+		translation_table[] = command.none;
+	}
+	command interpret_command_string(string cmd, bool ignore_invalid = true)
+	{
+		try
+		{
+			return cmd.to!command;
+		}
+		catch(ConvException e)
+		{
+			if (ignore_invalid)
+			{
+				stderr.writeln("Warning: Invalid command ignored: " ~ cmd);
+				return command.none;
+			}
+			else throw new Exception("Invalid command: " ~ cmd);
+		}
+	}
+}
